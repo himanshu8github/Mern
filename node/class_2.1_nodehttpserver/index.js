b7466f1bdde367eeb6451446af5f350e76f8fe8e
Himanshu Choudhary <himanshukakran8@gmail.com>
2025-08-14 22:32:02 +0530

    routing

---
+++ node/class_2.1_nodehttpserver/index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>Homepage</h1>');
+    } else if (req.url === '/about'){
+     res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>About</h1>');
+    }else if (req.url === '/contact'){
+     res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>Contact</h1>');
+    }else {
+     res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end('<h1>Not Found</h1>');
+    }
+});
+
+server.listen(5500, () => {
+    console.log("Server is running on 5500")
+})
